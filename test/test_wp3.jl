90ad44f7c9efb82dfde921c1de2705da857fe185
ladosamushia <colkhis@gmail.com>
2021-03-11 15:21:51 -0600

    minor fixes in the tests

--- test/test_wp3.jl
+++ test/test_wp3.jl
@@ -8,7 +8,7 @@ include("../src/wp3.jl")
 #@test distance([1.0, 1.0], [2000.0 - 2.0, 5.0], 2000.0) == 5.0
 #@test distance([1.0, 1.0], [4.0, 2000.0 - 3.0], 2000.0) == 5.0
 #@test distance([1.0, 1.0], [2000.0 - 2.0, 2000.0 - 3.0], 2000.0) == 5.0
-@test distance([4.0, 5.0], [1.0, 1.0], 2000.0) == 5.0
+@test distance([4.0, 5.0], [1.0, 1.0]) == 5.0
 #@test distance([2000.0 - 2.0, 5.0], [1.0, 1.0], 2000.0) == 5.0
 #@test distance([4.0, 2000.0 - 3.0], [1.0, 1.0], 2000.0) == 5.0
 #@test distance([2000.0 - 2.0, 2000.0 - 3.0], [1.0, 1.0], 2000.0) == 5.0
@@ -25,7 +25,7 @@ xy1 = [1 2 3 1999; 1 2 3 1999]
 xy2 = [1.01 2.01 3.01 1999.01; 1.01 2.01 3.01 1999.01]
 xy3 = [7 8 9; 7 8 9]
 
-histogram!(xy3, xy3, xy3, 2000.0, 1.0, hist)
+histogram!(xy3, xy3, xy3, 1.0, hist)
 for i in 1:10, j in 1:10, k in 1:10
     if (i == 2 && j == 2 && k == 3) || (i == 2 && j == 3 && k == 2) || (i == 3 && j == 2 && k == 2)
         @test hist[2,2,3] + hist[2,3,2] + hist[3,2,2] == 6
@@ -35,7 +35,7 @@ for i in 1:10, j in 1:10, k in 1:10
 end
 
 hist = zeros(Int, 10, 10, 10)
-histogram!(xy3, xy3, xy1, 2000.0, 1.0, hist)
+histogram!(xy3, xy3, xy1, 1.0, hist)
 hist_comp = zeros(Int, 10, 10, 10)
 hist_comp[2, 6, 7] = 1
 hist_comp[2, 7, 8] = 2
@@ -46,7 +46,7 @@ for i in 1:10, j in 1:10, k in 1:10
 end
 
 hist = zeros(Int, 10, 10, 10)
-histogram!(xy1, xy2, xy3, 2000.0, 1.0, hist)
+histogram!(xy1, xy2, xy3, 1.0, hist)
 hist_comp = zeros(Int, 10, 10, 10)
 hist_comp[1, 6, 6] = 1
 hist_comp[1, 8, 8] = 2
